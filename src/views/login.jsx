34ecaa4b75e3cc77543ec69a6c41a1202a568f56
Samuel Jesudunsin <jessam7007@gmail.com>
2019-04-11 11:48:28 +0100

    feat(login): add login page
    [Delivers #165273387]

---
+++ src/views/login.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Header from '../components/header';
+import Button from '../components/button';
+
+const Login = () => (
+  <>
+    <Header />
+    <section className="login">
+      <div className="container">
+        <h2>Log in</h2>
+        <form name="signup">
+          <div id="error" />
+          <div id="success" />
+          <input type="email" name="email" placeholder="Email" />
+          <input type="password" name="password" placeholder="password" />
+          <Button type="submit" id="submit" text="Login" />
+        </form>
+      </div>
+    </section>
+  </>
+);
+
+export default Login;
