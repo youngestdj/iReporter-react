5ab180bc602546e65c705610b46f9c7f8e2d6346
Samuel Jesudunsin <jessam7007@gmail.com>
2019-04-11 11:19:49 +0100

    feat(signup): add signup page
    [Delivers #165273373]

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import NotFound from '../views/404';
 import Home from '../views/home';
+import Signup from '../views/signup';
 
 const Routes = () => (
   <Switch>
     <Route path="/" exact component={Home} />
     <Route component={NotFound} />
+    <Route path="/signup" exact component={Signup} />
   </Switch>
 );
 
