bb89201c5996f8f11a09aa9755bebc400a50903c
Samuel Jesudunsin <jessam7007@gmail.com>
2019-04-18 17:03:52 +0100

    feat(signup): consume signup endpoint
    [Finishes #165442138]

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
+import '@babel/polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './assets/css/style.css';
 import App from './App';
+import store from './redux/store';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app')
+);
