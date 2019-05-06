903555e643b8d128eac285538eeeabd4399205c9
Samuel Jesudunsin <jessam7007@gmail.com>
2019-05-06 10:07:16 +0100

    chore(tests): add more tests
    - add axios mock file
    - add build badge to readme
    
    [Finishes #165818995]

---
+++ src/__mocks__/axios.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-undef */
+export default {
+  get: jest.fn(() => Promise.resolve({ data: {} })),
+  post: jest.fn(() => Promise.resolve({ data: {} })),
+  put: jest.fn(() => Promise.resolve({ data: {} })),
+  patch: jest.fn(() => Promise.resolve({ data: {} })),
+  delete: jest.fn(() => Promise.resolve({ data: {} })),
+};
+
+const axios = {
+  get: jest.fn(() => Promise.resolve({ data: {} })),
+  post: jest.fn(() => Promise.resolve({ data: {} })),
+  put: jest.fn(() => Promise.resolve({ data: {} })),
+  patch: jest.fn(() => Promise.resolve({ data: {} })),
+  delete: jest.fn(() => Promise.resolve({ data: {} }))
+};
+module.exports = axios;
